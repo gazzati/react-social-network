e68c84d69ad388c2eb70e8169dfd795dd54f2995
Timur <gazzaevtimur@gmail.com>
2020-04-27 00:49:12 +0300

    Updates

--- precache-manifest.04a256599f9b6d76d3a2df43d95ab7cb.js
+++ precache-manifest.04a256599f9b6d76d3a2df43d95ab7cb.js
@@ -1,35 +1,35 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "20ac77a9eb95182b65a3ccd9cbd69827",
+    "revision": "4832bb42fe2a8add09a5152d166c8618",
     "url": "/ReactSocialNetwork/index.html"
   },
   {
-    "revision": "33086b280c890972dc6c",
+    "revision": "899f6940ce8fb369cbb5",
     "url": "/ReactSocialNetwork/static/css/3.1c80b6f8.chunk.css"
   },
   {
-    "revision": "9a442e001ced88bd347f",
+    "revision": "c16a0b3154fe9994b62c",
     "url": "/ReactSocialNetwork/static/css/main.4f8a446f.chunk.css"
   },
   {
-    "revision": "ed68dbadf046d80ee3db",
-    "url": "/ReactSocialNetwork/static/js/2.f4d26905.chunk.js"
+    "revision": "988b4d1c9708b8997c50",
+    "url": "/ReactSocialNetwork/static/js/2.c4c9d609.chunk.js"
   },
   {
-    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
-    "url": "/ReactSocialNetwork/static/js/2.f4d26905.chunk.js.LICENSE.txt"
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/ReactSocialNetwork/static/js/2.c4c9d609.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "33086b280c890972dc6c",
-    "url": "/ReactSocialNetwork/static/js/3.c52a684c.chunk.js"
+    "revision": "899f6940ce8fb369cbb5",
+    "url": "/ReactSocialNetwork/static/js/3.05139d36.chunk.js"
   },
   {
-    "revision": "9a442e001ced88bd347f",
-    "url": "/ReactSocialNetwork/static/js/main.447cc442.chunk.js"
+    "revision": "c16a0b3154fe9994b62c",
+    "url": "/ReactSocialNetwork/static/js/main.59b1ad68.chunk.js"
   },
   {
-    "revision": "bee37647b33c733fd5cf",
-    "url": "/ReactSocialNetwork/static/js/runtime-main.3b945909.js"
+    "revision": "1da1b8ea93e6fe84f7d7",
+    "url": "/ReactSocialNetwork/static/js/runtime-main.fbe66523.js"
   },
   {
     "revision": "3ffaf718cec61743e91eaf7577bbb83c",
